0808836cdca8c3da0ad62126ce7b456b03349001
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-06-12 18:25:01 -0300

    Header, Hero, About, Brands

--- app.js
+++ app.js
@@ -28,3 +28,29 @@ function reveal() {
 }
 
 window.addEventListener("scroll", reveal);
+
+$(document).ready(function () {
+  $(".customer-logos").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+    ],
+  });
+});
